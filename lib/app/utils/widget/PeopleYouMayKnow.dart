27adc2b01be1c93ffa942f365d0e7eca648f0fda
Rizkyramadhan02 <ikyramadhan85@gmail.com>
2022-12-19 12:25:33 +0700

    Query Data Friends Firestore Database

--- lib/app/utils/widget/PeopleYouMayKnow.dart
+++ lib/app/utils/widget/PeopleYouMayKnow.dart
@@ -11,54 +11,68 @@ class PeopleYouMayKnow extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 200,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        shrinkWrap: true,
-        clipBehavior: Clip.antiAlias,
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Stack(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(30),
-                  child: const Image(
-                    image: NetworkImage(
-                        'https://pbs.twimg.com/media/EnbDAFKXcAAVBsO?format=jpg&name=large'),
-                  ),
-                ),
-                const Positioned(
-                  bottom: 10,
-                  left: 10,
-                  child: Text(
-                    'Cristiano Ronaldo',
-                    style: TextStyle(color: Colors.white, fontSize: 15),
-                  ),
-                ),
-                Positioned(
-                  bottom: 0,
-                  right: 0,
-                  child: SizedBox(
-                    height: 36,
-                    width: 36,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        padding: EdgeInsets.zero,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(40),
+      child: FutureBuilder(
+          future: authCon.getPeople(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            var data = snapshot.data!.docs;
+
+            return ListView.builder(
+              scrollDirection: Axis.horizontal,
+              shrinkWrap: true,
+              clipBehavior: Clip.antiAlias,
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                var hasil = data[index].data();
+                return Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Stack(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(30),
+                        child: Image(
+                          image: NetworkImage(hasil['photo']),
+                          height: 200,
+                          width: 200,
+                          fit: BoxFit.cover,
                         ),
                       ),
-                      child: const Icon(Iconsax.add_circle),
-                    ),
+                      Positioned(
+                        bottom: 10,
+                        left: 10,
+                        child: Text(
+                          hasil['name'],
+                          style: const TextStyle(
+                              color: Colors.white, fontSize: 15),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: SizedBox(
+                          height: 36,
+                          width: 36,
+                          child: ElevatedButton(
+                            onPressed: () => authCon.addFriends(hasil['email']),
+                            style: ElevatedButton.styleFrom(
+                              padding: EdgeInsets.zero,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(40),
+                              ),
+                            ),
+                            child: const Icon(Iconsax.add_circle),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ],
-            ),
-          );
-        },
-      ),
+                );
+              },
+            );
+          }),
     );
   }
 }
